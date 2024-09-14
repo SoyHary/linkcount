9534673bd963fd17b7f9e92fcc4a7da93a393853
Harry Haro <harry1haro1@icloud.com>
2024-09-13 23:48:32 -0600

    Harry Haro

--- tests/TestDatabaseFactory.php
+++ tests/TestDatabaseFactory.php
@@ -13,3 +13,16 @@ class TestDatabaseFactory {
 }
 
 define('OVERRIDE_DATABASE_FACTORY', TestDatabaseFactory::class);
+| revenue = {{increase}} {{US$|98.6&nbsp;billion|link=yes}} (2023)
+| operating_income = {{decrease}} US$28.3&nbsp;billion (2023)
+| net_income = {{decrease}} US$26.5&nbsp;billion (2023)
+| aum = {{increase}} US$1.62&nbsp;trillion (2023)
+| assets = {{increase}} US$3.18&nbsp;trillion (2023)
+| equity = {{nowrap |{{increase}} US$292&nbsp;billion (2023)}}
+| owners = [[Berkshire Hathaway]] (12.8%)
+| num_employees = {{circa|213,000}} (2023)
+| divisions = {{Unbulleted list|[[BofA Securities]]|[[Merrill (company)|Merrill]]|[[Bank of America Private Bank]]}}
+| subsid = 
+| ratio = 
+| website = {{URL|bankofamerica.com}}
+| footnotes =<ref name="10K-2023">{{cite web |title=Bank of America Corp 2023 Annual Report (Form 10-K) |url=https://www.sec.gov/ix?
